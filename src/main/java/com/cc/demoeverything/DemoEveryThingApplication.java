1a9c24a628972bf126c81184aedee7e709d70c62
cc <cx1939952554@163.com>
2022-07-23 21:29:39 +0800

    初始化

---
+++ src/main/java/com/cc/demoeverything/DemoEveryThingApplication.java
@@ -0,0 +1,13 @@
+package com.cc.demoeverything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEveryThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEveryThingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cc/demoeverything/DemoEveryThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.demoeverything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEveryThingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
